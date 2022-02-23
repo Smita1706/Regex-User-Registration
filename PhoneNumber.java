7faf3c01dcb4991ad99c16c82abfd1b2c5ff8259
Smita1706 <smitachinchkar1998@gmail.com>
2022-02-23 19:35:53 +0530

    Refactored : The use case 4 validate phone number is refactored

--- PhoneNumber.java
+++ PhoneNumber.java
@@ -1,19 +1,20 @@
 package com.bridgelabz.rejex;
-
-import java.util.Scanner;
-
+import java.util.*;
+import java.util.regex.*;
 public class PhoneNumber {
-	
 	private void validatePhoneNumber(String  number) {
-		String phNoPattern = "^(\\d{0,9}[\\s]?)?\\d{10}$";
-		boolean result = number.matches(phNoPattern);
-		System.out.println(result);
+		String p ="^[9][1]{0,1}\\s[0-9]{10}";
+		Pattern mob = Pattern.compile(p);
+		System.out.println(mob);
+		System.out.println(number);
+		Matcher result = mob.matcher(number);
+		System.out.println(result.matches());
 	}
-		public static void main(String[] arg) {
-			PhoneNumber phone = new PhoneNumber();
-			Scanner s = new Scanner(System.in);
-			System.out.println("Enter the phone number :");
-			String number = s.next();
-			phone.validatePhoneNumber(number);
-		}
+	public static void main(String[] arg) {
+		PhoneNumber phone = new PhoneNumber();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the phone number :");
+		String number = s.nextLine();
+		phone.validatePhoneNumber(number);
+	}
 }
